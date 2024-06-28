ba235317c83e182a491f7d0f29cc47bbd7c87017
VARSHA SWARAJ <126698394+callmeVAMP@users.noreply.github.com>
2024-06-28 20:50:44 +0530

    Create Home.jsx

---
+++ src/Pages/Home.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+//import Header from "../Components/Header";
+import Navbar from "../Components/Navbar";
+export default function Home() {
+  return (
+    <>
+      <div>
+        <Navbar />
+      </div>
+      
+    </>
+  );
+}
